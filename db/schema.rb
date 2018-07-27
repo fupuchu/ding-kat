de574e42b444713ff38c111ac56326b57937d55c
jellythewobbly <jellythewobbly@hotmail.com>
2018-07-27 16:26:05 +0800

    minor edit of schema

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2018_07_27_081115) do
     t.string "postal_code"
     t.date "subscription_start"
     t.date "subscription_end"
-    t.boolean "subscription_renew", default: true
+    t.boolean "subscription_renew", default: true, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
