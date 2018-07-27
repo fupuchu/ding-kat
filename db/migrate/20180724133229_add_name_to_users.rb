5fc088ec1508cc207a723cdb90d61456136f2325
jellythewobbly <jellythewobbly@hotmail.com>
2018-07-27 15:47:09 +0800

    Edited profile.html.erb, migration files

--- db/migrate/20180724133229_add_name_to_users.rb
+++ db/migrate/20180724133229_add_name_to_users.rb
@@ -2,7 +2,13 @@ class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :address_one, :string
+    add_column :users, :address_two, :string
+    add_column :users, :postal_code, :string
     add_reference :users, :subscription
-    # The code below is for active mailer to work DO NOT CHANGE
+    add_column :users, :subscription_start, :date
+    add_column :users, :subscription_end, :date
+    add_column :users, :subscription_renew, :boolean
   end
 end
