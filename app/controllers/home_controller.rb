65de1425bca0c287cef2364c714cd013b0246a6e
jellythewobbly <jellythewobbly@hotmail.com>
2018-07-29 06:58:23 +0800

    Added additional columns to the menus table, edited seeds.rb to seed additional data into tables, selection for menu and subscription both work now.
    Still working on overall UX for purchasing process.
    
    Things to do:
    1. Conditional redirecting based on already selected options for menu and subscription.
    2. Form for date/duration of subscription.
    3. Update subscription_start/subscription_end/subscription_renew columns on users table.
    4. Stripe/PayPal for payment method.
    
    Nice to have:
    1. Table for promo codes that user can apply to get a discount on their subscription.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,15 +28,29 @@ class HomeController < ApplicationController
     end
 
     def subscribe
-        @user = User.find(current_user.id)
-        @user.update_attribute(:subscription_id, params[:id])
-        redirect_to profile_path
+        if current_user != nil
+            @user = User.find(current_user.id)
+            @user.update_attribute(:subscription_id, params[:id])
+            redirect_to subscriptions_path
+        else
+            redirect_to new_user_session_path
+        end
     end
 
     def unsubscribe
         @user = User.find(current_user.id)
         @user.update_attribute(:subscription_id, nil)
         redirect_to subscriptions_path
+    end
+
+    def select_menu
+        if current_user != nil
+            @user = User.find(current_user.id)
+            @user.update_attribute(:menu_id, params[:id])
+            redirect_to menus_path
+        else
+            redirect_to new_user_session_path
+        end
     end
 
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,15 @@
 class MenusController < ApplicationController
     def index
         @menus = Menu.all
+        if current_user
+            if User.find(current_user.id) != nil
+                @user = User.find(current_user.id)
+                @current = User.find(current_user.id).menu_id
+                if @current != nil
+                    @menu = Menu.find(@user.menu_id)
+                end
+            end
+        end
     end
 
     def show
